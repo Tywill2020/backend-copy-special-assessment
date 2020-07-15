1ce1ec9a579ae31587b540f21cdabec6c36a7093
Tyrell Williams <tyrellwilliams2020@gmail.com>
2020-07-15 12:43:33 -0400

    2/3 complete

--- copyspecial.py
+++ copyspecial.py
@@ -7,40 +7,52 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Tyrell Williams, help from group session demo"
 
 import re
 import os
 import sys
-import shutil
+# import shutil
 import subprocess
 import argparse
 
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    paths = os.listdir(dirname)
+
+    special_paths = []
+    for filename in paths:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            special_paths.append(os.path.abspath(
+                os.path.join(dirname, filename)))
+    return special_paths
 
 
-def copy_to(path_list, dest_dir):
-    # your code here
-    return
+# def copy_to(path_list, dest_dir):
+# This function is not completed yet
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
-    return
+    """Zip special files"""
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    print(' '.join(cmd))
+    subprocess.check_output(cmd)
 
 
 def main(args):
+    print(args)
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='dest zipfile for special files')
     ns = parser.parse_args(args)
+    print(ns)
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
@@ -50,7 +62,10 @@ def main(args):
     # any required args, the general rule is to print a usage message and
     # exit(1).
 
-    # Your code here: Invoke (call) your functions
+    special_paths = get_special_paths(ns.from_dir)
+    print(special_paths)
+    # if ns.todir:
+    #     copy_to(special_paths, ns.todir)
 
 
 if __name__ == "__main__":
